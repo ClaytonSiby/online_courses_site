65dfe54b6e7dab2f9254cf4b99a6622ef68020e5
Clayton Siby <claytonsiby@gmail.com>
2021-09-20 23:45:51 +0200

    Add roles to the users table and migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_081133) do
+ActiveRecord::Schema.define(version: 2021_09_20_214409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2021_09_20_081133) do
     t.string "username"
     t.string "lastname"
     t.integer "age"
+    t.boolean "superadmin_role", default: false
+    t.boolean "supervisor_role", default: false
+    t.boolean "user_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
