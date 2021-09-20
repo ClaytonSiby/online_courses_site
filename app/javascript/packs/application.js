9fe2d673c442994535b038dbe56efb84b34ca7bd
Clayton Siby <claytonsiby@gmail.com>
2021-09-20 09:33:34 +0200

    Import the toastr gem in application.scss for notifications

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import 'bootstrap'
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import 'bootstrap';
+import '../../assets/stylesheets/application';
 
 global.toastr = require("toastr");
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
