8a784c0a63c8f3b63e7478373037853ab77c15ff
Clayton Siby <claytonsiby@gmail.com>
2021-09-21 01:31:39 +0200

    Add index and show pages for Users controller and update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
   devise_for :users
+  resources :users
 
   get 'sign_out', to: 'sessions#destroy', as: 'sign_out'
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
 end
