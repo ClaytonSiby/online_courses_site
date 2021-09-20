97741f108874b44d49324c59675b42bc3221e696
Clayton Siby <claytonsiby@gmail.com>
2021-09-20 09:30:19 +0200

    Initialize toastr gem configuration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
 
+global.toastr = require("toastr");
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
