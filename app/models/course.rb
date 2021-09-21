ff2a0ccaca4e90c534a165ccfa79ba57d0fdfb03
Clayton Siby <claytonsiby@gmail.com>
2021-09-21 15:44:41 +0200

    Establish an association between the User and Course models

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
   validates :image, presence: true
   validates :description, presence: true
 
+  belongs_to :user
   has_many :course_categories
 
   has_many :categories, through: :course_categories, dependent: :destroy
