17a361a56b84f1e807fcae19d75b213d09e4095f
Clayton Siby <claytonsiby@gmail.com>
2021-09-21 20:03:16 +0200

    Initialize the application carousel partial

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,7 @@ environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    Popper: ['@popperjs/core', 'default']
   })
 )
 
