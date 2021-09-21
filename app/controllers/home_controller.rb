bb44b025c35ad94a68031f75b8541c7b5bc50149
Clayton Siby <claytonsiby@gmail.com>
2021-09-21 16:27:47 +0200

    Relax the authentication requirements for the Home controller and update the view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
-    @users = User.all
+    @categories = Category.includes(:courses).all
   end
 end
